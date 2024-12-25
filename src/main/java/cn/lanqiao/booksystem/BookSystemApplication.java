61b4694c62409b6ad96b020ea9202116dc122ea0
guijuxiangshuiyu <3165889894@qq.com>
2024-12-25 09:35:23 +0800

    update project Struct

--- src/main/java/cn/lanqiao/booksystem/BookSystemApplication.java
+++ src/main/java/cn/lanqiao/booksystem/BookSystemApplication.java
@@ -1,9 +1,12 @@
 package cn.lanqiao.booksystem;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("cn.lanqiao.booksystem.mapper")
 public class BookSystemApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/cn/lanqiao/booksystem/service/BookService.java
@@ -0,0 +1,5 @@
+package cn.lanqiao.booksystem.service;
+
+public interface BookService {
+
+}
