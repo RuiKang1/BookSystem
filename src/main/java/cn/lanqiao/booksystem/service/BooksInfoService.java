e64197f161b42ce95100ebcb35d8eea0b51160f0
yituoyanba <1692303110@qq.com>
2024-12-25 09:57:52 +0800

    mybatisX了

--- src/main/java/cn/lanqiao/booksystem/service/BooksInfoService.java
+++ src/main/java/cn/lanqiao/booksystem/service/BooksInfoService.java
@@ -1,13 +1,12 @@
 package cn.lanqiao.booksystem.service;
 
-import generator.domain.BooksInfo;
-import com.baomidou.mybatisplus.extension.service.IService;
+
 
 /**
 * @author 一坨盐巴
 * @description 针对表【books_info】的数据库操作Service
 * @createDate 2024-12-25 09:32:54
 */
-public interface BooksInfoService extends IService<BooksInfo> {
+public interface BooksInfoService{
 
 }
