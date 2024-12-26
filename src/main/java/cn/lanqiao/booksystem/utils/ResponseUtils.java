3a64bd765dc631dd083797b1b94b8bcdef594cf4
guijuxiangshuiyu <3165889894@qq.com>
2024-12-26 10:40:03 +0800

    添加了响应工具类

---
+++ src/main/java/cn/lanqiao/booksystem/utils/ResponseUtils.java
@@ -0,0 +1,28 @@
+package cn.lanqiao.booksystem.utils;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author: 李某人
+ * @Date: 2024/09/12/11:24
+ * @Description:
+ */
+@Data
+@NoArgsConstructor
+public class ResponseUtils<T> {
+    private int code;//响应状态码 200 成功 304 失败
+    private String msg;//响应信息 登录成功 登录失败....
+    private T data;//响应数据 比如查询的数据，存放到data中
+
+    public ResponseUtils(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public ResponseUtils(int code, String msg, T data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+}
