291508d923b3a255d83d7a64f19b0f0ca81866aa
PEI <2521447753@qq.com>
2025-01-05 18:45:37 +0800

    书籍借阅代码

---
+++ src/main/java/cn/lanqiao/booksystem/mapper/BorrowingMapper.java
@@ -0,0 +1,16 @@
+package cn.lanqiao.booksystem.mapper;
+
+import cn.lanqiao.booksystem.modle.pojo.BookTypeInfo;
+import cn.lanqiao.booksystem.modle.pojo.BooksInfo;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface BorrowingMapper {
+    List<BookTypeInfo> getBookType();
+    List<BooksInfo> selectBooksByName(String name);
+    List<BooksInfo> selectBooksByAuthor(String author);
+    List<BooksInfo> selectBooksByType(String type);
+    List<BooksInfo> selectBooksByCard(String card);
+}
